38a835645b54c49dc0edf29eb81c287b3fa318b9
Peter Hillerström <peter.hillerstrom93@gmail.com>
2015-01-18 14:38:39 +0100

    Now links SDL2. Also added instructions on how to install on Windows.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
+#include <SDL.h>
+#undef main
 
 int main()
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* window = SDL_CreateWindow("Legend Of Cube", SDL_WINDOWPOS_UNDEFINED,
+	                                     SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_RESIZABLE);
+	if (window == NULL) {
+		std::cout << "Couldn't create window: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
 	if (boost::filesystem::exists("assets/this_is_not_an_asset.txt")) {
 		std::cout << "Found assets\n";
 	} else {
 		std::cout << "Couldn't find assets\n";
 	}
-	std::cin.get();
+
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
