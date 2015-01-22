d7e69099e4d3db4c30942f6c27a2788937cebd71
Peter Hillerström <peter.hillerstrom93@gmail.com>
2015-01-22 20:44:56 +0100

    Incomplete very simple test program to see if Ogre works properly.

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,26 +3,47 @@
 #include <SDL.h>
 #undef main
 
+#include <string>
+#include <memory>
+#include "Ogre.h"
+
 int main()
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	/*SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window* window = SDL_CreateWindow("Legend Of Cube", SDL_WINDOWPOS_UNDEFINED,
-	                                     SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_RESIZABLE);
+	SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_RESIZABLE);
 	if (window == NULL) {
-		std::cout << "Couldn't create window: " << SDL_GetError() << std::endl;
-		return 1;
+	std::cout << "Couldn't create window: " << SDL_GetError() << std::endl;
+	return 1;
 	}
 
 	if (boost::filesystem::exists("assets/this_is_not_an_asset.txt")) {
-		std::cout << "Found assets\n";
+	std::cout << "Found assets\n";
 	} else {
-		std::cout << "Couldn't find assets\n";
+	std::cout << "Couldn't find assets\n";
 	}
 
 	SDL_Delay(3000);
 
 	SDL_DestroyWindow(window);
-	SDL_Quit();
+	SDL_Quit();*/
+
+	std::string resourcePath = ""; // Should possibly be different on Mac OS X.
+
+	std::unique_ptr<Ogre::Root> root{new Ogre::Root{resourcePath + "plugins.cfg", resourcePath + "ogre.cfg", "Ogre.log"}};
+	if (!root->showConfigDialog()) return -1;
+
+	/*Ogre::ConfigFile cfg;
+	cfg.load(resourcePath + "resources.cfg");
+
+	Ogre::ConfigFile::SectionIterator secItr = cfg.getSectionIterator();
+
+	Ogre::String secName, typeName, archName;
+	while (secItr.hasMoreElements()) {
+		secName = secItr.peekNextKey();
+
+	}*/
+	
 	return 0;
 }
