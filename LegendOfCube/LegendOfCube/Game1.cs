a2776992aa9c8680c9fc7ddfa74d785cbf9ea6a3
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-01-28 20:43:23 +0100

    Better sample code

--- LegendOfCube/LegendOfCube/Game1.cs
+++ LegendOfCube/LegendOfCube/Game1.cs
@@ -11,6 +11,13 @@ using Microsoft.Xna.Framework.Media;
 
 namespace LegendOfCube
 {
+    struct Cube
+    {
+        public VertexPositionColor[] verts;
+        public DynamicVertexBuffer vertexBuffer;
+        public DynamicIndexBuffer indexBuffer;
+        public Matrix modelToWorld;
+    }
     /// <summary>
     /// This is the main type for your game
     /// </summary>
@@ -19,9 +26,31 @@ namespace LegendOfCube
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
         private Model barrelModel;
-        private Texture2D barrelTexture;
         private BasicEffect basicEffect;
         private float rotation;
+        private Texture2D cubeImage;
+        private Cube cube;
+
+        private readonly ushort[] cubeIndices =
+    		{
+    			0,2,1, // -x
+    			1,2,3,
+
+    			4,5,6, // +x
+    			5,7,6,
+
+    			0,1,5, // -y
+    			0,5,4,
+
+    			2,6,7, // +y
+    			2,7,3,
+
+    			0,4,6, // -z
+    			0,6,2,
+
+    			1,3,7, // +z
+    			1,7,5,
+    		};
 
         public Game1()
         {
@@ -37,9 +66,28 @@ namespace LegendOfCube
         /// </summary>
         protected override void Initialize()
         {
+            Window.AllowUserResizing = true;
             graphics.PreferMultiSampling = true;
+            GraphicsDevice.BlendState = BlendState.Opaque;
 
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
             basicEffect = new BasicEffect(graphics.GraphicsDevice);
+
+            cube = new Cube();
+            cube.verts = new VertexPositionColor[8];
+            cube.verts[0] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, -0.5f), new Color(0.0f, 0.0f, 0.0f));
+            cube.verts[1] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, 0.5f), new Color(0.0f, 0.0f, 1.0f));
+            cube.verts[2] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, -0.5f), new Color(0.0f, 1.0f, 0.0f));
+            cube.verts[3] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, 0.5f), new Color(0.0f, 1.0f, 1.0f));
+            cube.verts[4] = new VertexPositionColor(new Vector3(0.5f, -0.5f, -0.5f), new Color(1.0f, 0.0f, 0.0f));
+            cube.verts[5] = new VertexPositionColor(new Vector3(0.5f, -0.5f, 0.5f), new Color(1.0f, 0.0f, 1.0f));
+            cube.verts[6] = new VertexPositionColor(new Vector3(0.5f, 0.5f, -0.5f), new Color(1.0f, 1.0f, 0.0f));
+            cube.verts[7] = new VertexPositionColor(new Vector3(0.5f, 0.5f, 0.5f), new Color(1.0f, 1.0f, 1.0f));
+
+            cube.vertexBuffer = new DynamicVertexBuffer(graphics.GraphicsDevice, typeof(VertexPositionColor), 8, BufferUsage.WriteOnly);
+            cube.indexBuffer = new DynamicIndexBuffer(graphics.GraphicsDevice, typeof(ushort), 36, BufferUsage.WriteOnly);
+
+            cube.modelToWorld = Matrix.Identity;
 
             base.Initialize();
         }
@@ -53,7 +101,7 @@ namespace LegendOfCube
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             barrelModel = Content.Load<Model>("barrel");
-            barrelTexture = Content.Load<Texture2D>("ExplosiveBarrel_Reference");
+            cubeImage = Content.Load<Texture2D>("cube");
             // TODO: use this.Content to load your game content here
         }
 
@@ -74,7 +122,8 @@ namespace LegendOfCube
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
                 this.Exit();
             }
 
@@ -90,28 +139,40 @@ namespace LegendOfCube
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
             float fov = 45;
 
-            rotation += 0.045f;
+            cube.modelToWorld *= Matrix.CreateRotationY(0.045f);
 
-            Matrix world = Matrix.CreateScale(0.1f) * Matrix.CreateRotationY(rotation);
             Matrix view = Matrix.CreateLookAt(new Vector3(5f, 3f, 0f), Vector3.Zero, new Vector3(0f, 1f, 0f));
             Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(fov), GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000f);
 
-            basicEffect.World = world;
+            cube.vertexBuffer.SetData(cube.verts, 0, 8, SetDataOptions.Discard);
+            cube.indexBuffer.SetData(cubeIndices, 0, 36, SetDataOptions.Discard);
+            GraphicsDevice.Indices = cube.indexBuffer;
+            GraphicsDevice.SetVertexBuffer(cube.vertexBuffer);
+
+            basicEffect.World = cube.modelToWorld;
             basicEffect.View = view;
             basicEffect.Projection = projection;
-            basicEffect.TextureEnabled = true;
-            basicEffect.Texture = barrelTexture;
-            basicEffect.EnableDefaultLighting();
+            basicEffect.VertexColorEnabled = true;
+
             foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                barrelModel.Draw(world, view, projection);
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 8, 0, 36);
             }
+
+            Matrix barrelWorld = Matrix.CreateTranslation(0f, 0f, -15f) * Matrix.CreateScale(0.1f);
+            barrelModel.Draw(barrelWorld, view, projection);
+
             spriteBatch.Begin();
-            spriteBatch.Draw(barrelTexture, new Rectangle(0, 0, 200, 200), Color.White);
+            spriteBatch.Draw(cubeImage, new Rectangle(0, 0, 200, 200), Color.White);
             spriteBatch.End();
+
+            base.Draw(gameTime);
         }
     }
 }
