0be05b2a184916c9b15db298e6fd6ad6812f9f82
coffer <emma@goding.eu>
2015-02-05 08:24:27 +0100

    stupit git

--- LegendOfCube/LegendOfCube/Engine/LegendOfCubeGame.cs
+++ LegendOfCube/LegendOfCube/Engine/LegendOfCubeGame.cs
@@ -78,7 +78,7 @@ namespace LegendOfCube.Engine
 			world.Models[barrelEntity.Id] = Content.Load<Model>("cube_plain");
 			for (int i = 0; i < barrels.Length; i++)
 			{
-				world.Models[barrels[i].Id] = Content.Load<Model>("barrel");	
+				world.Models[barrels[i].Id] = Content.Load<Model>("cube_1x1g_wood");	
 			}
 
 		}
