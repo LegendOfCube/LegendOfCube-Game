43ce9e4703fa9365736792f90369f81be000c9e4
lucaspi <lucaspersson93@gmail.com>
2015-02-05 14:54:38 +0100

    created gameplaysystem

---
+++ LegendOfCube/LegendOfCube/Engine/GameplaySystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfCube.Engine
+{
+	class GameplaySystem
+	{
+		// Constants
+		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+		private static readonly Properties MOVEMENT_INPUT = new Properties(Properties.TRANSFORM |
+																				Properties.INPUT_FLAG);
+
+		public void applyInputData(World world)
+		{
+
+		}
+	}
+}
