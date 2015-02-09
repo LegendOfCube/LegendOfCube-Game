8a41966c7b2192931aef8e0efb04a7b5794190a4
Wiixtor <wiixtor@gmail.com>
2015-02-09 16:02:06 +0100

    Created struct PlayerCubeState

---
+++ LegendOfCube/LegendOfCube/Engine/PlayerCubeState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendOfCube.Engine
+{
+	/// <summary>
+	/// Contains information that only the cube needs.
+	/// </summary>
+	public struct PlayerCubeState
+	{
+		public const int MAXJUMPS = 2;
+		public int currentJumps;
+	}
+}
