26c62fad5b7294a9ff422c79f04697f0539653fc
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-02-09 13:13:49 +0100

    Spaces to tabs

--- LegendOfCube/LegendOfCube/Engine/PhysicsSystem.cs
+++ LegendOfCube/LegendOfCube/Engine/PhysicsSystem.cs
@@ -21,42 +21,42 @@ namespace LegendOfCube.Engine
 		                                                         Properties.VELOCITY);
 
 		private static readonly Properties HAS_FRICTION = new Properties(
-														 Properties.TRANSFORM |
-														 Properties.VELOCITY |
-														 Properties.FRICTION_FLAG);
+		                                                 Properties.TRANSFORM |
+		                                                 Properties.VELOCITY |
+		                                                 Properties.FRICTION_FLAG);
 
-        private static readonly Vector3 GRAVITY = new Vector3(0.0f, -9.82f, 0.0f);
+		private static readonly Vector3 GRAVITY = new Vector3(0.0f, -9.82f, 0.0f);
 		private static readonly float MAX_VELOCITY = 15f;
 
 		public void ApplyPhysics(float delta, World world)
 		{
 			for (UInt32 i = 0; i < world.MaxNumEntities; i++)
 			{
-                Properties properties = world.EntityProperties[i];
-                // Check if velocity should be updated
-                if (properties.Satisfies(ACCELERATABLE))
-                {
-                    world.Velocities[i] += (world.Accelerations[i] * delta);
+				Properties properties = world.EntityProperties[i];
+				// Check if velocity should be updated
+				if (properties.Satisfies(ACCELERATABLE))
+				{
+					world.Velocities[i] += (world.Accelerations[i] * delta);
 					if (world.Velocities[i].Length() > MAX_VELOCITY)
 					{
-						Vector2 temp = new Vector2(world.Velocities[i].X,world.Velocities[i].Z);
+						Vector2 temp = new Vector2(world.Velocities[i].X, world.Velocities[i].Z);
 						temp.Normalize();
 						temp *= MAX_VELOCITY;
 						world.Velocities[i].X = temp.X;
 						world.Velocities[i].Z = temp.Y;
 					}
-                }
-
-                // Apply gravity
-                if (properties.Satisfies(HAS_GRAVITY))
-                {
-                    world.Velocities[i] += (GRAVITY * delta);
 				}
 
-                // Update position
-                if (properties.Satisfies(MOVABLE))
-                {
-                    world.Transforms[i].Translation += (world.Velocities[i] * delta);
+				// Apply gravity
+				if (properties.Satisfies(HAS_GRAVITY))
+				{
+					world.Velocities[i] += (GRAVITY * delta);
+				}
+
+				// Update position
+				if (properties.Satisfies(MOVABLE))
+				{
+					world.Transforms[i].Translation += (world.Velocities[i] * delta);
 					// Hacky floor
 					if (world.Transforms[i].Translation.Y < 0)
 					{
