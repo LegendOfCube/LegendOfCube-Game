5e05c9f754254ce4383d8b3446b379d5ee161448
Wiixtor <wiixtor@gmail.com>
2015-02-17 10:43:11 +0100

    Initial screen

---
+++ LegendOfCube/LegendOfCube/Engine/GameScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LegendOfCube.Engine.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfCube.Engine
+{
+	class GameScreen : Screen
+	{
+		public GameScreen(RenderSystem renderSystem, Game game) : base(renderSystem, game)
+		{
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ LegendOfCube/LegendOfCube/Engine/Screen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LegendOfCube.Engine.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfCube.Engine
+{
+	abstract class Screen
+	{
+		private Game game;
+		private RenderSystem renderSystem;
+
+		protected Screen(RenderSystem renderSystem, Game game)
+		{
+			this.renderSystem = renderSystem;
+			this.game = game;
+		}
+
+		protected abstract void Update(GameTime gameTime);
+
+		protected abstract void Draw(GameTime gameTime);
+	}
+}
