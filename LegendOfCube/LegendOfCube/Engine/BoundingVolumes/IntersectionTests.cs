fbc103bbfc6c8e2cf72134408d2073f80604821e
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-02-18 19:04:11 +0100

    Move Matrix3x3.cs to CubeMath folder

--- LegendOfCube/LegendOfCube/Engine/BoundingVolumes/IntersectionTests.cs
+++ LegendOfCube/LegendOfCube/Engine/BoundingVolumes/IntersectionTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using LegendOfCube.Engine.CubeMath;
 using Microsoft.Xna.Framework;
 
 namespace LegendOfCube.Engine.BoundingVolumes
