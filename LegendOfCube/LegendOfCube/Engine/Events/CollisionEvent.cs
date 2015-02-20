a86af361f464fc247494429db0605f4536225d1f
Wiixtor <wiixtor@gmail.com>
2015-02-20 13:37:49 +0100

    Added EventBuffer and CollisionEvent

---
+++ LegendOfCube/LegendOfCube/Engine/Events/CollisionEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendOfCube.Engine.Events
+{
+	struct CollisionEvent
+	{
+		public Entity Collider;
+		public Entity Colided;
+	}
+}
---
+++ LegendOfCube/LegendOfCube/Engine/Events/EventBuffer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendOfCube.Engine.Events
+{
+	class EventBuffer
+	{
+		public readonly List<CollisionEvent> CollisionEvents;
+
+		public EventBuffer()
+		{
+			CollisionEvents = new List<CollisionEvent>(100);
+		}
+
+		public bool AddEvent(ref CollisionEvent e)
+		{
+			this.CollisionEvents.Add(e);
+			return true;
+		}
+
+		public void Flush()
+		{
+			CollisionEvents.Clear();
+		}
+	}
+}
