6b19675688d08ed89c4cba8865abeeeb99eb83e3
Wiixtor <wiixtor@gmail.com>
2015-02-20 13:54:21 +0100

    Basic EventSystem

---
+++ LegendOfCube/LegendOfCube/Engine/EventSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LegendOfCube.Engine.Events;
+
+namespace LegendOfCube.Engine
+{
+	class EventSystem
+	{
+		public void HandleEvents(World world)
+		{
+			EventBuffer eventBuffer = world.EventBuffer;
+			foreach (var collisionEvent in eventBuffer.CollisionEvents)
+			{
+				if (world.EntityProperties[collisionEvent.Colided.Id].Satisfies(Properties.MODEL) && 
+					world.EntityProperties[collisionEvent.Collider.Id].Satisfies(Properties.ACCELERATION)
+				{
+					
+				}
+
+			}
+		}
+	}
+}
