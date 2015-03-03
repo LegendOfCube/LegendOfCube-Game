b8063bc3d6c4c3bc44ec9f274db40af7c6d9026d
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-03-03 15:40:47 +0100

    StandardEffect stuff in concept level

--- LegendOfCube/LegendOfCube/Levels/ConceptLevel.cs
+++ LegendOfCube/LegendOfCube/Levels/ConceptLevel.cs
@@ -13,8 +13,6 @@ namespace LegendOfCube.Levels
 		private static Entity[] walls;
 		private static Entity DeathZone;
 
-		
-
 		public static void CreateLevel(World world, Game game)
 		{
 			platforms = new Entity[10];
@@ -23,23 +21,26 @@ namespace LegendOfCube.Levels
 			var cubeModel = game.Content.Load<Model>("Models/Cube/cube_clean");
 			var platformModel = game.Content.Load<Model>("Models/Platform/platform");
 			var wallModel = game.Content.Load<Model>("Models/Brick_Wall/brick_wall");
-	
+
 			var playerEffect = new StandardEffectParams
 			{
-				//DiffuseTexture = Game.Content.Load<Texture>("Models/Cube/cube_diff"),
+				DiffuseColor = Color.Gray.ToVector4(),
 				EmissiveTexture = game.Content.Load<Texture>("Models/Cube/cube_emissive"),
 				SpecularColor = Color.Gray.ToVector4(),
 				EmissiveColor = Color.White.ToVector4()
 			};
 
-			var otherCubeEffect = new StandardEffectParams
+			var platformEffect = new StandardEffectParams
 			{
-				//DiffuseTexture = Game.Content.Load<Texture>("Models/Cube/cube_diff"),
-				//SpecularTexture = Game.Content.Load<Texture>("Models/Cube/cube_specular"),
-				//EmissiveTexture = Game.Content.Load<Texture>("Models/Cube/cube_emissive"),
-				//NormalTexture = Game.Content.Load<Texture>("Models/Cube/cube_normal"),
-				//SpecularColor = Color.White.ToVector4(),
-				//EmissiveColor = Color.White.ToVector4()
+				DiffuseTexture = game.Content.Load<Texture>("Models/Platform/rusted metal-d"),
+				NormalTexture = game.Content.Load<Texture>("Models/Platform/rust_normal_sharp"),
+				SpecularColor = Color.Gray.ToVector4()
+			};
+
+			var wallEffect = new StandardEffectParams
+			{
+				DiffuseTexture = game.Content.Load<Texture>("Models/Brick_Wall/brick_d"),
+				NormalTexture = game.Content.Load<Texture>("Models/Brick_Wall/brick_n_sharp")
 			};
 
 			world.SpawnPoint = new Vector3(0, 5, 0);
@@ -60,7 +61,8 @@ namespace LegendOfCube.Levels
 			platforms[0] =
 				new EntityBuilder().WithModel(platformModel)
 					.WithPosition(new Vector3(0, 0, 0))
-					.WithBoundingVolume(new OBB(new Vector3(0,-0.25f,0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(10,0.5f,10)))
+					.WithStandardEffectParams(platformEffect)
+					.WithBoundingVolume(new OBB(new Vector3(0, -0.25f, 0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(10, 0.5f, 10)))
 					.AddToWorld(world);
 
 			// Walls and platform to test length gaining wall jumps
@@ -68,6 +70,7 @@ namespace LegendOfCube.Levels
 				new EntityBuilder().WithModel(wallModel)
 					.WithTransform(Matrix.CreateScale(2))
 					.WithPosition(new Vector3(-5, 3, 20))
+					.WithStandardEffectParams(wallEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0, 1.25f, 0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(0.5f, 2.5f, 5)))
 					.AddToWorld(world);
 
@@ -75,12 +78,14 @@ namespace LegendOfCube.Levels
 				new EntityBuilder().WithModel(wallModel)
 					.WithTransform(Matrix.CreateScale(2))
 					.WithPosition(new Vector3(10, 6, 40))
+					.WithStandardEffectParams(wallEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0, 1.25f, 0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(0.5f, 2.5f, 5)))
 					.AddToWorld(world);
 
 			platforms[1] =
 				new EntityBuilder().WithModel(platformModel)
 					.WithPosition(new Vector3(0, 9, 60))
+					.WithStandardEffectParams(platformEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0, -.25f, 0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(10,0.5f,10)))
 					.AddToWorld(world);
 
@@ -88,6 +93,7 @@ namespace LegendOfCube.Levels
 			platforms[2] =
 				new EntityBuilder().WithModel(platformModel)
 					.WithPosition(new Vector3(0, 0, -25))
+					.WithStandardEffectParams(platformEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0, -.25f, 0), Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ, new Vector3(10,0.5f,10)))
 					.AddToWorld(world);
 
@@ -96,12 +102,14 @@ namespace LegendOfCube.Levels
 				new EntityBuilder().WithModel(wallModel)
 					.WithTransform(Matrix.CreateScale(2))
 					.WithPosition(new Vector3(10, 3, 0))
+					.WithStandardEffectParams(wallEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0,1.25f,0),Vector3.UnitX,Vector3.UnitY,Vector3.UnitZ,new Vector3(0.5f, 2.5f, 5)))
 					.AddToWorld(world);
 
 			platforms[3] =
 				new EntityBuilder().WithModel(platformModel)
 					.WithPosition(new Vector3(0, 12, 0))
+					.WithStandardEffectParams(platformEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0,-.25f,0),Vector3.UnitX,Vector3.UnitY,Vector3.UnitZ,new Vector3(10,.5f,10)))
 					.AddToWorld(world);
 
@@ -109,7 +117,8 @@ namespace LegendOfCube.Levels
 			platforms[4] =
 				new EntityBuilder().WithModel(platformModel)
 					.WithPosition(new Vector3(-45, 0, 0))
-					.WithVelocity(Vector3.UnitX*8, 0)
+					.WithVelocity(Vector3.UnitX * 8, 0)
+					.WithStandardEffectParams(platformEffect)
 					.WithBoundingVolume(new OBB(new Vector3(0,-.25f,0),Vector3.UnitX,Vector3.UnitY,Vector3.UnitZ,new Vector3(10,.5f,10)))
 					.WithAI(new Vector3[] { new Vector3(-45, 0, 0), new Vector3(-20, 0, 0), new Vector3(-45, 25, 0)}, true)
 					.AddToWorld(world);
@@ -120,6 +129,12 @@ namespace LegendOfCube.Levels
 					.WithBoundingVolume(new OBB(new Vector3(0, 0.5f, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1), new Vector3(1, 1, 1)))
 					.WithAdditionalProperties(new Properties(Properties.DEATH_ZONE_FLAG))
 					.AddToWorld(world);
+			world.LightPosition = new Vector3
+			{
+				X = -300,
+				Y = 100,
+				Z = 0
+			};
 		}
 	}
 }
