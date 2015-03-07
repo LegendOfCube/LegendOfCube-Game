5e142ea40fa2a4650e001a27db1341a338d0bb26
Peter Hillerström <peter@hstroem.se>
2015-03-07 23:35:15 +0100

    Improved calculation of cube state, now has an angle variable that separates what counts as wall and what counts as ground.

--- LegendOfCube/LegendOfCube/Engine/EventSystem.cs
+++ LegendOfCube/LegendOfCube/Engine/EventSystem.cs
@@ -5,13 +5,15 @@ using System.Security.Policy;
 using System.Text;
 using LegendOfCube.Engine.Events;
 using Microsoft.Xna.Framework;
+using System.Diagnostics;
 
 namespace LegendOfCube.Engine
 {
 	class EventSystem
 	{
-		private const float ON_GROUND_LIMIT = 0.70f;
-		private const float ON_WALL_LIMIT = 0.90f;
+		private static readonly float GROUND_WALL_ANGLE = MathHelper.ToRadians(75.0f); // 0 < angle < 90
+		private static readonly float ON_WALL_LIMIT = (float)Math.Sin(GROUND_WALL_ANGLE);
+		private static readonly float ON_GROUND_LIMIT = (float)Math.Cos(GROUND_WALL_ANGLE);
 
 		public static void CalculateCubeState(World world)
 		{
@@ -24,30 +26,20 @@ namespace LegendOfCube.Engine
 			// Check if player cube collided with any walls or grounds
 			foreach (var e in world.EventBuffer.CollisionEvents)
 			{
-				if (e.Collider.Id == world.Player.Id)
+				float xDot = Math.Abs(e.Axis.X);
+				float zDot = Math.Abs(e.Axis.Z);
+				float wallDot = (float)Math.Sqrt(xDot * xDot + zDot * zDot);
+				if (e.Collider.Id == world.Player.Id || e.CollidedWith.Id == world.Player.Id)
 				{
-					if (Math.Abs(e.Axis.Y) > ON_GROUND_LIMIT)
+					if (e.Axis.Y > ON_GROUND_LIMIT)
 					{
 						world.PlayerCubeState.OnGround = true;
-						world.PlayerCubeState.GroundAxis = e.Axis;
+						world.PlayerCubeState.GroundAxis = (e.Collider.Id == world.Player.Id ? 1.0f : -1.0f) * e.Axis;
 					}
-					else if ((Math.Abs(e.Axis.X) + Math.Abs(e.Axis.Z)) > ON_WALL_LIMIT)
+					else if (wallDot > ON_WALL_LIMIT)
 					{
 						world.PlayerCubeState.OnWall = true;
-						world.PlayerCubeState.WallAxis = e.Axis;
-					}
-				}
-				else if (e.CollidedWith.Id == world.Player.Id)
-				{
-					if (Math.Abs(e.Axis.Y) > ON_GROUND_LIMIT)
-					{
-						world.PlayerCubeState.OnGround = true;
-						world.PlayerCubeState.GroundAxis = -e.Axis;
-					}
-					else if ((Math.Abs(e.Axis.X) + Math.Abs(e.Axis.Z)) > ON_WALL_LIMIT)
-					{
-						world.PlayerCubeState.OnWall = true;
-						world.PlayerCubeState.WallAxis = -e.Axis;
+						world.PlayerCubeState.WallAxis = (e.Collider.Id == world.Player.Id ? 1.0f : -1.0f) * e.Axis;
 					}
 				}
 			}
