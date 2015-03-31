54a892d7449d99211f54429c4e6e2e20e83593d0
Wiixtor <wiixtor@gmail.com>
2015-03-31 16:21:48 +0200

    Increased air movement

--- LegendOfCube/LegendOfCube/Engine/MovementSystem.cs
+++ LegendOfCube/LegendOfCube/Engine/MovementSystem.cs
@@ -13,7 +13,7 @@ namespace LegendOfCube.Engine
 
 		// Movement constants
 		private const float MOVEMENT_ACCELERATION = 35.0f;
-		private const float MOVEMENT_AIR_ACCELERATION = 10.0f;
+		private const float MOVEMENT_AIR_ACCELERATION = 20.0f;
 		private const float WALL_ANTI_GRAVITY_FACTOR = 0.5f;
 		private const float ROTATIONAL_SPEED = 360;
 		private static readonly float ROTATIONAL_SPEED_RAD = MathHelper.ToRadians(ROTATIONAL_SPEED);
