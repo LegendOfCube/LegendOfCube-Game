eda37bd7a8606228496df23d7d90650f933358c7
coffer <christoffer.matsson@gmail.com>
2015-04-09 15:15:08 +0200

    second wall slide adjusted

--- LegendOfCube/LegendOfCube/Levels/Level1.cs
+++ LegendOfCube/LegendOfCube/Levels/Level1.cs
@@ -83,7 +83,7 @@ namespace LegendOfCube.Levels
 				* Matrix.CreateRotationZ(MathHelper.ToRadians(90))).WithPosition(-3, -27, 88).AddToWorld(world);
 			hangingPlatformBuilder.Copy().WithPosition(-45, -27, 88).AddToWorld(world);
 
-			brickWallArrowsHBuilder.Copy().WithTransform(Matrix.CreateScale(5)).WithPosition(-55, -27, 70).AddToWorld(world);
+			brickWallArrowsHBuilder.Copy().WithTransform(Matrix.CreateScale(5)).WithPosition(-60, -27, 70).AddToWorld(world);
 			
 			/*dropSignBuilder.Copy().WithTransform(Matrix.CreateScale(4, 4, 4) * Matrix.CreateRotationY(MathHelper.ToRadians(90))
 				* Matrix.CreateRotationZ(MathHelper.ToRadians(-90))).WithPosition(58, -40, 45).AddToWorld(world);
