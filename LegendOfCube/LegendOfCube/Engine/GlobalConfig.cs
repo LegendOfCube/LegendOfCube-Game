773f13a29d4277019386f074ac56db011b459dd5
Peter Hillerström <peter@hstroem.se>
2015-04-13 11:21:41 +0200

    Skeleton GlobalConfig class. Now to fill it with settings and connect them to logic.

---
+++ LegendOfCube/LegendOfCube/Engine/GlobalConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendOfCube.Engine
+{
+	public class GlobalConfig
+	{
+		// Singleton instance
+		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+		private static readonly GlobalConfig instance = new GlobalConfig();
+		public static GlobalConfig Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		// Settings
+		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+		// Public methods
+		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+		public bool LoadFromFile()
+		{
+			// TODO: Not yet implemented.
+			return false;
+		}
+
+		public bool SaveToFile()
+		{
+			// TODO: Not yet implemented.
+			return false;
+		}
+
+		// Constructor
+		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+		private GlobalConfig()
+		{
+			
+		}
+	}
+}
