b90df374e654f891eb4564669245d94dd4d736d9
Wiixtor <wiixtor@gmail.com>
2015-04-13 14:10:48 +0200

    Reconstructed level 13 to work with new movementSystem

--- LegendOfCube/LegendOfCube/Levels/Level13.cs
+++ LegendOfCube/LegendOfCube/Levels/Level13.cs
@@ -13,7 +13,7 @@ namespace LegendOfCube.Levels
 		{
 			World world = new World(1000);
 
-			world.SpawnPoint = new Vector3(0, 5, 0);
+			world.SpawnPoint = new Vector3(0, 2, 0);
 			world.LightDirection = Vector3.Normalize(new Vector3(3.0f, -1.0f, -3.0f));
 			world.CameraPosition = world.SpawnPoint + new Vector3(-3, 0, 0);
 			world.AmbientIntensity = 0.45f;
@@ -42,7 +42,7 @@ namespace LegendOfCube.Levels
 				.WithModelData(contentCollection.BrickWall);
 
 			var sawTrap = wallBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(1, 2, 1)*Matrix.CreateRotationY(MathHelper.ToRadians(90))*
+				.WithTransform(Matrix.CreateScale(0.5f, 1, 0.5f)*Matrix.CreateRotationY(MathHelper.ToRadians(90))*
 				               Matrix.CreateRotationZ(MathHelper.ToRadians(45)))
 				.WithAdditionalProperties(new Properties(Properties.DEATH_ZONE_FLAG))
 				.WithStandardEffectParams(new StandardEffectParams()
@@ -57,129 +57,84 @@ namespace LegendOfCube.Levels
 			 */
 
 			// Starting platform
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(6, 5, 1.5f))
-				.WithPosition(Vector3.Zero)
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(4, 2.5f, 1.5f))
+				.WithPosition(Vector3.Zero).AddToWorld(world);
 
 			// First Wall
-			wallBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(5,30,3))
-				.WithPosition(new Vector3(30, 0, 0))
-				.AddToWorld(world);
+			wallBuilder.Copy().WithTransform(Matrix.CreateScale(2.5f,15,3f))
+				.WithPosition(new Vector3(20, 0, 0)).AddToWorld(world);
 
 			// Second platform
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(4, 7.5f, 1.5f))
-				.WithPosition(new Vector3(-10, 15f, 0))
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(3.3f, 5f, 1.5f))
+				.WithPosition(new Vector3(-4f, 8f, 0)).AddToWorld(world);
 
 			// First saw deathtrap
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, -6))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, -4))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, -2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, 0))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, 2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, 4f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 13, 6))
-				.AddToWorld(world);
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, -6)).AddToWorld(world);
 
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, -4)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, -2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, 0)).AddToWorld(world);
+			
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, 2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, 4f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(10, 7, 6)).AddToWorld(world);
 			// Second Wall
-			wallBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(5, 30, 3))
-				.WithPosition(new Vector3(-30, 0, 0))
-				.AddToWorld(world);
+			wallBuilder.Copy().WithTransform(Matrix.CreateScale(2.5f, 15, 3))
+				.WithPosition(new Vector3(-20, 0, 0)).AddToWorld(world);
 
 			// Third platform
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(4, 7.5f, 1.5f))
-				.WithPosition(new Vector3(10, 30f, 0))
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(3.3f, 5f, 1.5f))
+				.WithPosition(new Vector3(4, 18f, 0)).AddToWorld(world);
 
 			// Second saw deathtrap
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, -6))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, -4))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, -2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, 0))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, 2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, 4f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(20, 28, 6))
-				.AddToWorld(world);
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, -6)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, -4)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, -2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, 0)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, 2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, 4f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(15, 17, 6)).AddToWorld(world);
 
 			// Fourth platform
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(2, 5, 1.5f))
-				.WithPosition(new Vector3(0, 45f, 0))
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(2, 5, 1.5f))
+				.WithPosition(new Vector3(0, 27.5f, 0)).AddToWorld(world);
 
 			// Third saw deathtrap
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, -6))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, -4))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, -2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, 0))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, 2f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, 4f))
-				.AddToWorld(world);
-			sawTrap.Copy()
-				.WithPosition(new Vector3(8, 43, 6))
-				.AddToWorld(world);
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, -6)).AddToWorld(world);
 
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, -4)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, -2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, 0)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, 2f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, 4f)).AddToWorld(world);
+
+			sawTrap.Copy().WithPosition(new Vector3(8, 26, 6)).AddToWorld(world);
 
 			// Finnish 
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(2, 2.5f, 1.5f))
-				.WithPosition(new Vector3(-20, 43.75f, 0))
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(1, 2.5f, 1.5f))
+				.WithPosition(new Vector3(-15, 26.25f, 0)).AddToWorld(world);
 
-			platformBuilder.Copy()
-				.WithTransform(Matrix.CreateScale(2, 2.5f, 1.5f))
-				.WithPosition(new Vector3(-20, 45f, 0))
-				.WithAdditionalProperties(new Properties(Properties.WIN_ZONE_FLAG))
-				.AddToWorld(world);
+			platformBuilder.Copy().WithTransform(Matrix.CreateScale(1, 2.5f, 1.5f)).WithPosition(new Vector3(-15, 27.5f, 0))
+				.WithAdditionalProperties(new Properties(Properties.WIN_ZONE_FLAG)).AddToWorld(world);
 
 			//Falling death
-			new EntityBuilder()
-				.WithPosition(new Vector3(0, -100.0f, 0))
+			new EntityBuilder().WithPosition(new Vector3(0, -100.0f, 0))
 				.WithBoundingVolume(new OBB(new Vector3(0, 0.5f, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1), new Vector3(1000, 1, 1000)))
-				.WithAdditionalProperties(new Properties(Properties.DEATH_ZONE_FLAG))
-				.AddToWorld(world);
+				.WithAdditionalProperties(new Properties(Properties.DEATH_ZONE_FLAG)).AddToWorld(world);
 
 			return world;
 		}
