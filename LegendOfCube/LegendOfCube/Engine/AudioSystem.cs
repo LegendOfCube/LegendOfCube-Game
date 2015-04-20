4540f11ba2670f81190808c10adc64b339b4e581
Wiixtor <wiixtor@gmail.com>
2015-04-20 10:38:32 +0200

    Created AudioSystem

---
+++ LegendOfCube/LegendOfCube/Engine/AudioSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+namespace LegendOfCube.Engine
+{
+	class AudioSystem
+	{
+
+		private readonly SoundEffect TestSlap;
+		public AudioSystem(ContentManager cm)
+		{
+
+		}
+		public static void PlayTestSlap()
+		{
+			
+		}
+	}
+}
