7bd5ee31900c8cd6089dad73080063ccd125b343
Wiixtor <wiixtor@gmail.com>
2015-04-22 11:56:34 +0200

    read and write to file implemented

--- LegendOfCube/LegendOfCube/Engine/Highscore.cs
+++ LegendOfCube/LegendOfCube/Engine/Highscore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace LegendOfCube.Engine
 	public class Highscore
 	{
 		private static readonly Highscore INSTANCE = new Highscore();
+		private readonly string path;
 
 		public static Highscore Instance
 		{
@@ -19,10 +21,10 @@ namespace LegendOfCube.Engine
 
 		public Highscore()
 		{
-			if (!LoadFromFile())
-			{
-				highScores = new Dictionary<string, List<float>>();
-			}
+			path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+			           @"\My Games\Legend of Cube\HighScores.txt";
+			highScores = new Dictionary<string, List<float>>();
+			LoadFromFile();
 		}
 
 		public void AddHighScore(string name, float time)
@@ -38,6 +40,7 @@ namespace LegendOfCube.Engine
 			{
 				highScores.Add(name, new List<float>{time});
 			}
+			SaveToFile();
 		}
 
 		public List<float> GetHighScoresForLevel(string name)
@@ -52,12 +55,41 @@ namespace LegendOfCube.Engine
 
 		public void SaveToFile()
 		{
-			
+			StringBuilder sb = new StringBuilder();
+			foreach (var level in highScores.Keys)
+			{
+				sb.Append(level);
+				foreach (var time in highScores[level])
+				{
+					sb.Append(",");
+					sb.Append(time);
+				}
+				sb.Append("\n");
+			}
+			File.WriteAllText(path, sb.ToString());
 		}
 
 		public bool LoadFromFile()
 		{
-			return false;
+			string textFromFile;
+			try
+			{
+				textFromFile = File.ReadAllText(path);
+			}
+			catch (FileNotFoundException)
+			{
+				return false;
+			}
+			var lines = textFromFile.Split('\n');
+			foreach (var line in lines)
+			{
+				var scores = line.Split(',');
+				for (int i = 1; i < scores.Length; i++)
+				{
+					AddHighScore(scores[0],float.Parse(scores[i]));
+				}
+			}
+			return true;
 		}
 	}
 }
