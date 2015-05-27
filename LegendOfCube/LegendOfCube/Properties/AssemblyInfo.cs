ec7c294359cc33a86baded3531f44347baae086d
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-05-27 16:35:24 +0200

    Bump version number

--- LegendOfCube/LegendOfCube/Properties/AssemblyInfo.cs
+++ LegendOfCube/LegendOfCube/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // Will automatically be appended with .0 for unspecified numbers
-[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyVersion("0.3")]
 
 // Free-form version name (example: "1.0 RC1")
-[assembly: AssemblyInformationalVersion("0.2")]
+[assembly: AssemblyInformationalVersion("0.3")]
